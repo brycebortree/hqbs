3b0d681cec2eef850cf6c82fc3ee1faae12e47cd
brytree <brycebortree@gmail.com>
2016-06-21 18:59:33 -0700

    users validation

---
+++ models/user.js
@@ -0,0 +1,65 @@
+'use strict';
+
+var bcrypt = require('bcrypt');
+
+module.exports = function(sequelize, DataTypes) {
+  var user = sequelize.define('user', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [1, 99],
+          msg: 'Name must be between 1 and 99 characters'
+        }
+      }
+    },
+      email: {
+        type: DataTypes.STRING,
+        validate: {
+          isEmail: {
+            msg: 'Invalid email address'
+          }
+        }
+      },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        len: {
+          args: [8, 99],
+          msg: 'Password must be between 8 and 99 characters'
+        }
+      }
+    }
+  }, {
+    classMethods: {
+      associate: function(models) {
+        // associations can be defined here
+      },
+      authenticate: function(email, password, callback) {
+      this.find({
+        where: {email:email}
+      }).then(function(user) {
+        if (!user) callback(null, false);
+        bcrypt.compare(password, user.password, function(err, result) {
+          if (err) return callback(err);
+          callback(null, result ? user : false);
+        });
+      }).catch(callback);
+    }
+  },
+    hooks: {
+      beforeCreate: function(user, options, callback) {
+        if (user.password) {
+          bcrypt.hash(user.password, 10, function(err, hash) {
+            if (err) return callback(err);
+            user.password = hash;
+            callback(null, user);
+          });
+        } else {
+          callback(null, user);
+        }
+      }
+    }
+  });
+  return user;
+};
